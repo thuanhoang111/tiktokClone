a765f58a408c05705206092b840dd0c4bac9e1bd
Thuan Hoang <thamthuy100272@gmail.com>
2022-07-04 17:28:50 +0700

    finish logo tiktok and custom searcg spacer

--- src/components/Layouts/DefaultLayout/Header/index.js
+++ src/components/Layouts/DefaultLayout/Header/index.js
@@ -1,11 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import classNames from 'classnames/bind';
 import style from './Header.module.scss';
-
+import images from '~/assets/image';
 const cx = classNames.bind(style);
 function Header() {
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('inner')}></div>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="ko có hình " />
+        </div>
+        <div className={cx('search')}>
+          <input placeholder="search accounts and video" spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={cx('search-btn')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx('actions')}></div>
+      </div>
     </header>
   );
 }
